06fa9122bd7926cd7593c87d07dc9367de6ce71a
Ying <ying__li@hotmail.com>
2022-11-20 18:21:14 -0500

    adding places view with mock data

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,7 @@ function error404 () {
         <Def>
             <main>
                 <h1>404: PAGE NOUT FOUND</h1>
-                <p>Oops, sorru, we can't find this page!</p>
+                <p>Oops, sorry, we can't find this page!</p>
             </main>
         </Def>
     )
