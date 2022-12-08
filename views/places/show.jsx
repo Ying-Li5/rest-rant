6d3d32e333886aff9ca78a7b0bf35dc6621d02de
Ying <ying__li@hotmail.com>
2022-12-07 23:49:08 -0500

    insert average rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated.
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating =(
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -38,9 +52,8 @@ function show (data) {
                 
                 <div className='col-sm-6'>
                     <h3>Rating</h3>
-                    <p>Not Rated</p>
+                    <h2>{rating}</h2>
                 </div>
-
                 <div className='col-sm-6'>
                     <h2>Description</h2>
                     <h3> {data.place.showEstablished()} </h3>
